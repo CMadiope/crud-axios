3f31f6c844ef9a4617b59e5a26a089f5e8f71716
Confidence Madiope <confidencem97@gmail.com>
2022-08-17 15:04:57 +0200

    ADD: state management to Create component

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { FormCheck } from "react-bootstrap";
 
 const Create = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
+
   return (
-    <Form>
+    <Form className='create-form'>
       <Form.Group className='mb-3'>
         <Form.Label>First Name</Form.Label>
-        <Form.Control placeholder='First Name' size='lg' />
+        <Form.Control
+          placeholder='First Name'
+          size='lg'
+          onChange={(e) => setFirstName(e.target.value)}
+        />
       </Form.Group>
       <Form.Group className='mb-3'>
         <Form.Label>Last Name</Form.Label>
-        <Form.Control placeholder='Last Name' />
+        <Form.Control
+          placeholder='Last Name'
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </Form.Group>
       <Form.Group className='mb-3'>
-        <FormCheck label='I agree with the Terms and Conditions' />
+        <FormCheck
+          label='I agree with the Terms and Conditions'
+          onChange={(e) => setCheckbox(!checkbox)}
+        />
       </Form.Group>
 
       <Button size='lg' type='submit'>
